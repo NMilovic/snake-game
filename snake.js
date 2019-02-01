c579f918bd326264af898e648af374562005636a
Nikola Milovic <nikolamilovic62@gmail.com>
2019-02-01 17:12:55 +0100

    Apple getCoordinate function and drawing Apple

--- snake.js
+++ snake.js
@@ -26,10 +26,42 @@ let bonusApple = {};
 let direction = 'right';
 let speed = 200;
 
+//Apple functions
+//First function will be simple, it will take two numbers and randomly output value between them
+function random(min, max){
+    return Math.random() * (max - min) + min;
+} 
+
+//Because of frequent use of below expression I decided to put it in function
+function getAppleCoordinates (min, max){
+    return Math.round(random(min, max) / min) * min; //expression
+}
+
+//Secound will generate coordinates for apple object
+function createApple(){
+    //Here we need to give apple object some coordinates so it can be drawn in canvas
+    apple.x = getAppleCoordinates(size, canvas.width - size);
+    apple.y = getAppleCoordinates(size, canvas.height - size);
+
+
+    //Also, here we can prevent apple spawning on any position that snake takes
+    for(let i = 0; i < snake.length; i += 1){
+        let s = snake[i];
+        if(s.x === apple.x && s.y === apple.y){
+            apple.x = getAppleCoordinates(size, canvas.width - size);
+            apple.y = getAppleCoordinates(size, canvas.height - size);
+        }
+    }
+}
+
 //Here will be defined draw function which is going to redraw canvas and make some animation
 function draw(){
     //Firstly, lets clear whole canvas and afte that lets draw everything
     ctx.clearRect(0, 0, canvas.width, canvas.height); //this will be clearing canvas
+
+    //Draw apple
+    ctx.fillStyle = '#B70431';
+    ctx.fillRect(apple.x, apple.y, size, size);
 
     //Draw snake. Actually, here we need to walk through sanke array and draw part by part
     for(let i = 0; i < snake.length; i += 1){
@@ -40,5 +72,7 @@ function draw(){
 
 }
 
+//Calling createApple function so it can generate coordinates
+createApple();
 //Requesting animation
 reqAnimation = requestAnimationFrame(draw);
